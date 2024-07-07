36943768d13ebd112265aa0f0840b2d00ff59554
ShalvaKvi <102624754+ShalvaKvi@users.noreply.github.com>
2024-07-07 10:55:49 +0300

    Update battle_interaction.R

--- testing_ground/battle_interaction.R
+++ testing_ground/battle_interaction.R
@@ -6,12 +6,12 @@ library(f1dataR)
 data_raw <- f1dataR::load_session_laps(season = 2024, round = 11, session = "R")
 
 
-
+# pivot the df to long format - one row per sector
 long_df <- data_raw |>
   select(!sector1session_time:sector3session_time,) |> 
   pivot_longer(
     cols = sector1time:sector3time,
-    names_to = "sector",
+    names_to = "sector_number",
     names_pattern = "(\\d+)",
     values_to = "sector_time",
   ) |> 
@@ -20,15 +20,23 @@ long_df <- data_raw |>
       select(!sector1time:sector3time) |> 
       pivot_longer(
         cols = sector1session_time:sector3session_time,
-        names_to = "sector",
+        names_to = "sector_number",
         names_pattern = "(\\d+)",
         values_to = "sector_session_time",
       ) |> 
-      select(driver, lap_number, sector, sector_session_time),
-    by = c("driver", "lap_number", "sector")
-  ) |> 
-  group_by(driver) |> 
-  mutate(runing_time = ifelse)
+      select(driver, lap_number, sector_number, sector_session_time),
+    by = c("driver", "lap_number", "sector_number")
+  )
+
+
+rank_df <- long_df |> 
+  group_by(lap_number, sector_number) |> 
+  arrange(sector_session_time) |> 
+  mutate(sector_position = row_number()) |> 
+  arrange(lap_number, sector_number, position) |> 
+  select(driver, lap_number,lap_time, sector_number,  sector_number, position, sector_position, sector_session_time, lap_start_time)
+  #group_by(driver) |> 
+  #mutate(runing_time = ifelse)
 
 
 test <- data_raw |> group_by(driver) |> filter(row_number() == 1) |> mutate(a = sector2time+sector3time)
